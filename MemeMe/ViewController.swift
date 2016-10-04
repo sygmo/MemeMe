e881d64e11664c44dfb29f244ad19939d64ee0e1
sygmo <sydney.mercier91@gmail.com>
2016-10-04 15:03:13 -0500

    Added camera functionality

--- MemeMe/ViewController.swift
+++ MemeMe/ViewController.swift
@@ -22,10 +22,35 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         // Dispose of any resources that can be recreated.
     }
 
-    @IBAction func pickAnImage(_ sender: AnyObject) {
+    @IBAction func pickAnImageFromAlbum(_ sender: AnyObject) {
         let imagePicker = UIImagePickerController()
         imagePicker.delegate = self
+        imagePicker.sourceType = UIImagePickerControllerSourceType.photoLibrary
         self.present(imagePicker, animated: true, completion: nil)
+    }
+    
+    @IBAction func pickAnImageFromCamera(_ sender: AnyObject) {
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        imagePicker.sourceType = UIImagePickerControllerSourceType.camera
+        self.present(imagePicker, animated: true, completion: nil)
+    }
+    
+    
+    func imagePickerController(_: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        
+        print("In imagePickerController")
+        if let image = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            imageView.image = image
+            self.dismiss(animated: true, completion: nil)
+        }
+        
+    }
+    
+    func imagePickerControllerDidCancel(_: UIImagePickerController){
+        
+        print("In imagePickerControllerDidCancel")
+        dismiss(animated: true, completion: nil)
     }
 
 }
